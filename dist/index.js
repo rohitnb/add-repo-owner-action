631904b53508c6d9f1f02313085c4aff52255926
Rohit N B <48172220+rohitnb@users.noreply.github.com>
2021-12-07 10:38:59 +0000

    initial commit

--- dist/index.js
+++ dist/index.js
@@ -11482,33 +11482,30 @@ const axios = __nccwpck_require__(6284);
 
 const targetRepoName = core.getInput('repo-name');
 const ghToken = core.getInput('org-admin-token');
-var createRepoData = JSON.stringify(
+const newOwner = core.getInput('new-owner');
+var addOwnerData = JSON.stringify(
   {
-    "name":targetRepoName,
-    "private":true,
-    "visibility":"private"
+    "permission": "admin"
   }
 );
 const targetOrgName = github.context.payload.repository.owner.login;
 
 var config = {
-  method: 'post',
-  url: 'https://api.github.com/orgs/'+targetOrgName+'/repos',
+  method: 'put',
+  url: 'https://api.github.com/repos/'+targetOrgName+'/'+targetRepoName+'/collaborators/'+newOwner,
   headers: { 
     'Accept': 'application/vnd.github.v3+json', 
     'Authorization': 'token '+ghToken, 
     'Content-Type': 'application/json'
   },
-  data : createRepoData
+  data : addOwnerData
 };
 
 axios(config)
 .then(function (response) {
-  console.log("Repo "+targetRepoName+' created successfully!');
-  core.setOutput("repo-url", "https://github.com/"+targetOrgName+"/"+targetRepoName);
+  console.log("User "+newOwner+' add as owner successfully!');
 })
 .catch(function (error) {
-  core.setOutput("repo-url", "");
   core.setFailed(error.message);
 });
 })();
